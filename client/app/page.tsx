22299887a5c6c31bf3a70764c2e4cf47e2435a27
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-07-19 14:45:23 +0100

    Added dynamic imports

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,16 @@
-import About from "@/components/About";
+import dynamic from "next/dynamic";
 import Hero from "@/components/Hero";
-import Products from "@/components/Products";
-import Sponsors from "@/components/Sponsors";
 import styles from "./page.module.css";
+const About = dynamic(() => import("@/components/About"), {
+  suspense: true,
+});
+const Products = dynamic(() => import("@/components/Products"), {
+  suspense: true,
+  ssr: false,
+});
+const Sponsors = dynamic(() => import("@/components/Sponsors"), {
+  suspense: true,
+});
 
 export default function Home() {
   return (
