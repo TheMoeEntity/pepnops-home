c7df4df689591579f9bf51cea94cf215eee7ce0c
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-07-22 09:43:15 +0100

    updates

--- client/app/contact/ContactForm.tsx
+++ client/app/contact/ContactForm.tsx
@@ -1,24 +1,9 @@
+import Image from "next/image";
 import styles from "../../components/index.module.css";
+// import "./contact.scss";
 
 const ContactForm = () => {
-  return (
-    <div className={styles.contactForm}>
-      <div className={styles.itHero}>
-        <div className={styles.centered}>
-          <h1>
-            Innovation<span>. </span>Collaboration<span>. </span>Excellence
-            <span>. </span>
-          </h1>
-          <h1 className={styles.capt3}>
-            In {`today's`} rapidly evolving technological landscape, companies,
-            including startups, are faced with constant advancements and
-            innovations that can significantly impact their operations.
-          </h1>
-        </div>
-        <div className={styles.overlay}></div>
-      </div>
-    </div>
-  );
+  return <div className={styles.contactForm}></div>;
 };
 
 export default ContactForm;
