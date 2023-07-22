ba97f6294a2f0536735d1bb864abc74428481c23
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-07-22 09:43:26 +0100

    updates

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Layout from "@/components/layout";
+import "bootstrap/dist/css/bootstrap.css";
 const inter = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
