06634da1c56df710b6d10065b5245143a319c976
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-07-27 11:07:32 +0100

    changed the tech team picture

--- client/app/careers/CareersPage.tsx
+++ client/app/careers/CareersPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 import styles from "../../components/index.module.css";
 import Image from "next/image";
-import mac from "../../public/images/tech.jpeg";
+import mac from "../../public/images/tech3.png";
 import man from "../../public/images/cons3_1.webp";
 import Link from "next/link";
 import TypeEffect from "./TypeEffect";
@@ -15,29 +15,11 @@ const CareersPage = () => {
   return (
     <div className={styles.careers}>
       <div className={styles.careerHero}>
-        {/* <Image
-          src={man}
-          alt="hero-image"
-          fill={true}
-          style={{ backgroundPosition: "left", backgroundSize: "cover" }}
-          quality={100}
-          priority={true}
-          sizes="(max-width: 1575px) 100vw, (max-width: 1920px) 50vw, 33vw"
-        /> */}
         <div className={styles.overlay}></div>
         <div className={styles.centered}>
           <h1>
             <TypeEffect />
           </h1>
-          {/* <p className={styles.capt4}>
-            At Pepnops, we believe that people and their opinions matter. We are
-            passionate about creating a collaborative and inclusive work
-            environment where {`everyone's`} voice is valued. We understand that
-            our success as a company is built upon the unique perspectives and
-            contributions of each team member. {`That's`} why we are dedicated
-            to fostering an atmosphere that encourages open communication,
-            creativity, and growth.
-          </p> */}
         </div>
       </div>
       <div className={styles.why}>
@@ -55,7 +37,7 @@ const CareersPage = () => {
             />
           </div>
           <div>
-            <h2>PEPNOPS Inc.</h2>
+            <h2>You Matter!</h2>
             <p>
               At Pepnops, we believe that people and their opinions matter. We
               are passionate about creating a collaborative and inclusive work
